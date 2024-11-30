9bfd7576884d53eb9b5f04314dc02d4e05eb08f2
Felippe Scheidt <felippescheidt@gmail.com>
2024-11-30 13:55:24 -0300

    update urls

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,21 @@
 <script>
-    import { base } from '$app/paths';
-    import "./app.css"
-    let {children} = $props();
-    </script>
-    
+import { base } from '$app/paths';
+import "./app.css"
+let {children} = $props();
+</script>
+
+<nav>
+    <a href={`${base}/`}>home</a>
+    <a href={`${base}/dogs`}>dogs</a>
+</nav>
+
+<div class="content">
+    {@render children()}
+</div>
+
+<footer>
     <nav>
-      <a href={`${base}/`}>home</a>
-      <a href={`${base}/dogs`}>dogs</a>
+    <a href="https://fscheidt.github.io/dogs" title="GitHub Pages" target="_blank">gh pages</a>
+    <a href="https://github.com/fscheidt/dogs" title="Project source-code" target="_blank">source</a>
     </nav>
-    
-    <div class="content">
-      {@render children()}
-    </div>
-    
-    <footer>
-      <nav>
-        <a href="https://fscheidt.github.io/css3" title="GitHub Pages" target="_blank">gh pages</a>
-        <a href="https://github.com/fscheidt/css3" title="Project source-code" target="_blank">source</a>
-      </nav>
-    </footer>
-    
+</footer>
