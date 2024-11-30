4160b906031cc5a7fa3e8e9961239484a15dfeb0
Felippe Scheidt <felippescheidt@gmail.com>
2024-11-30 17:18:22 -0300

    update packages

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
+import { mdsvex } from 'mdsvex'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', '.svx', '.md'], // Add .svx/.md to recognized extensions
+	preprocess: mdsvex({ extensions: ['.svx', '.md'] }),
 	kit: {
 		paths: {
 			base: process.argv.includes('dev') ? '' : `/dogs`  // <= project name
