bf610c4ea0a9f196a5d807e19ea63ede867fb9bb
Felippe Scheidt <felippescheidt@gmail.com>
2024-12-08 17:11:29 -0300

    testing user session in svelte stores

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,19 @@
 <script>
+import {user} from "$lib/app.svelte.js";
 import dogs from '$lib/data/dogs.json';
 import Dog from "$lib/Dog.svelte";
+const userPrefs = $user || {
+    search: ""
+}
 </script>
 
 <main>
     <header>
         <h1 id="title">Dogs</h1>
     </header>
+
+    <input type="text" bind:value={userPrefs.search}>
+    <pre>{JSON.stringify(userPrefs, null, 2)}</pre>
 
     <p style="font-style:italic;">from <a href="https://dog.ceo/dog-api/" target="_blank">dog API</a></p>
     
